6a973df98c0e9ce52785f4cb7e09bf60491bbdf9
rohithsiddhartha <rohithsiddhartha22@gmail.com>
2024-07-10 04:26:32 -0400

    Pipeline folder formatting

--- code_files/quick_html_to_text.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-
-def extract_text_from_html(html_content):
-    """Extract text from HTML content using BeautifulSoup."""
-    soup = BeautifulSoup(html_content, 'html.parser')
-    text = soup.get_text(separator=' ')
-    return text
